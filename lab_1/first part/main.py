bdc66570397e7b39aab3970356cbf18650d625ed
mappywin <mappydesuu@gmail.com>
2025-03-09 15:05:21 +0400

    added function to write ciphertext to file

--- lab_1/first part/main.py
+++ lab_1/first part/main.py
@@ -1,17 +1,25 @@
+from idlelib.iomenu import encoding
+
 from  vigenere import *
 
 def read_text(filename: str) -> str:
 
     with open(filename, 'r', encoding='utf-8') as text:
-        return text.read()
+        return text.read()\
+
+def write_encrypted_text(filename: str, text: str) -> None:
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
 
 def main():
 
     key = "собака"
-    input_text = read_text("user_texts/cipher_text.txt")
+    input_text = read_text("user_texts/input_text.txt")
+    encrypted_text = vigenere_cipher(input_text, key)
+    output_text = "user_texts/output_text.txt"
 
-    print(vigenere_cipher(input_text, key))
-
+    write_encrypted_text(output_text, encrypted_text)
 
 if __name__ == "__main__":
      main()

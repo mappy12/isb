39c54e03c3aa25f1acb72e65888280ca689395aa
mappywin <mappydesuu@gmail.com>
2025-03-09 15:52:14 +0400

    added parser

--- lab_1/first part/main.py
+++ lab_1/first part/main.py
@@ -1,27 +1,44 @@
+from argparse import Namespace
 from idlelib.iomenu import encoding
+import argparse
 
 from  vigenere import *
+
+
+def parser_create() -> Namespace:
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('input_text', type=str, help='Name of input text file')
+    parser.add_argument('output_text', type=str, help='Name of output text file')
+    parser.add_argument('key', type=str, help='Filename containing the key')
+
+    return parser.parse_args()
+
 
 def read_text(filename: str) -> str:
 
     with open(filename, 'r', encoding='utf-8') as text:
-        return text.read()\
+        return text.read()
+
 
 def write_encrypted_text(filename: str, text: str) -> None:
 
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(text)
 
+
 def main():
 
-    key = "собака"
-    input_text = read_text("user_texts/input_text.txt")
+    args = parser_create()
+
+    key = read_text(args.key)
+    input_text = read_text(args.input_text)
     encrypted_text = vigenere_cipher_encrypt(input_text, key)
-    output_text = "user_texts/output_text.txt"
+    output_text = args.output_text
 
     write_encrypted_text(output_text, encrypted_text)
 
-    print(vigenere_cipher_decrypt(encrypted_text, key))
 
 if __name__ == "__main__":
      main()
